13919d98f755c00b8e830d38938041915a041624
Stelios <loggidistel@gmail.com>
2025-05-06 14:28:53 +0300

    αλλαγή του welcomeText

--- src/main/java/application/walliedev/HelloController.java
+++ src/main/java/application/walliedev/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to GitHub!");
+        welcomeText.setText("Welcome to GitHub! ");
     }
 }
