df0f17a99cc2ee08d33e0a1b161d2f7ca1c4fd90
John Charalampidis <91800123+j0hn-char@users.noreply.github.com>
2025-05-24 14:04:36 +0300

    Revert "Email/giannis"

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module application.walliedev {
     requires MaterialFX;
     requires java.sql;
     requires mysql.connector.j;
-    requires jakarta.mail;
+
 
     opens application.walliedev to javafx.fxml;
     exports application.walliedev;
